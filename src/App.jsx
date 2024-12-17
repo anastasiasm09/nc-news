89060ac51750026446dca700c8ed2ea4ff21d106
Anastasiia Samoilova <anastasiasm09@gmail.com>
2024-12-17 10:03:25 +0000

    Added articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Articles from './components/Articles'
+import Topics from './components/Topics'
+import Users from './components/Users'
+
+
 
 function App() {
+  const appTitle = "TITLE"
 
-  return
+  return (
+    <>
+      <div>
+          <Header className="title" title={appTitle} />
+          <Routes>
+            <Route path='/articles' element={<Articles />} />
+            <Route path='/topics' element={<Topics />} />
+            <Route path='/my-accaunt' element={<Users />} />
+          </Routes>
+      </div>
+  </>
+  )
 }
 
 export default App
---
+++ src/components/Topics.jsx
@@ -0,0 +1,5 @@
+function Topics() {
+    return <p>Topics</p>
+}
+
+export default Topics
